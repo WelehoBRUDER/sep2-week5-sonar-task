c876a6e99debb473bc1667f4d1a5ee7af0bdc1eb
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-11-20 14:04:11 +0200

    idk did somethng

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
+    final public static Logger log = Logger.getLogger(App.class.getName());
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       App.log.info("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        App.log.info("How many loops would you like?");
+        int loops = sc.nextInt();
+
+        App.iterateLoop(1, loops);
+    }
+
+    public static void iterateLoop(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            final int current = i;
+            log.info(() -> "i = " + current);
         }
     }
 }
---
+++ src/test/java/TestApp.java
@@ -0,0 +1,14 @@
+import org.example.App;
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+
+    @Test
+    void testMain() {
+
+    }
+    @Test
+    void testIterateLoop() {
+        App.iterateLoop(1, 3);
+    }
+}
