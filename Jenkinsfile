822b6683ae15f8fcd0d5c62d31d1e6a06a228744
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-11-20 14:33:37 +0200

    improved test coverage

--- Jenkinsfile
+++ Jenkinsfile
@@ -40,6 +40,8 @@ pipeline {
                                 -Dsonar.host.url=http://localhost:9000 ^
                                 -Dsonar.login=${env.SONAR_TOKEN} ^
                                 -Dsonar.java.binaries=target/classes
+                                -Dsonar.sources = src/main/java
+                                -Dsonar.tests = src/test/java
                             """
                         }
                     }
--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,16 +1,28 @@
 import org.example.App;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestApp {
 
     @Test
-    void testMain() {
-
+    void main() {
+        String[] args = {};
+        String mockData = "4\n";
+        ByteArrayInputStream mockInput = new ByteArrayInputStream(mockData.getBytes());
+        System.setIn(mockInput);
+        App.main(args);
     }
 
     @Test
     void testFindInt() {
-
+        String mockData = "8\n";
+        ByteArrayInputStream mockInput = new ByteArrayInputStream(mockData.getBytes());
+        System.setIn(mockInput);
+        int num = App.findInt();
+        assertEquals(8, num);
     }
 
     @Test
